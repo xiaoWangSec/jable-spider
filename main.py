307ef9c15b1b6ad527a4064e897e8ed4ed117496
xiaoWangSec <124417044+xiaoWangSec@users.noreply.github.com>
2023-02-22 12:38:43 +0000

    update

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ db = "AV.db"
 
 if __name__ == '__main__':
 
-    for i in tqdm(range(1, 35), colour='green', desc='进度: '):
+    for i in tqdm(range(1, 5), colour='green', desc='进度: '):
         time.sleep(1)
         # 最新上市
 
-        # url = f"https://fs1.app/new-release/?mode=async&function=get_block&block_id=list_videos_common_videos_list&sort_by=release_year&from={str(i)}"
+        url = f"https://fs1.app/new-release/?mode=async&function=get_block&block_id=list_videos_common_videos_list&sort_by=release_year&from={str(i)}"
         # 最近更新
 
-        url = f"https://fs1.app/latest-updates/?mode=async&function=get_block&block_id=list_videos_latest_videos_list&sort_by=post_date&from={str(i)}"
+        # url = f"https://fs1.app/latest-updates/?mode=async&function=get_block&block_id=list_videos_latest_videos_list&sort_by=post_date&from={str(i)}"
         content = session.get(url, timeout=20).text
 
         soup = BeautifulSoup(content, 'lxml')
