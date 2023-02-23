b2d4ca6414c1bab18d9d3e4d458ba762a05485cf
xiaoWangSec <124417044+xiaoWangSec@users.noreply.github.com>
2023-02-23 15:38:54 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const inner = document.getElementById("content");
+let output = ""
+let globalDB
+config = {
+	locateFile: filename => `./dist/sql-wasm.wasm`
+}
+initSqlJs(config).then(function (SQL) {
+	const xhr = new XMLHttpRequest();
+	// https://raw.githubusercontent.com/xiaoWangSec/jable-spider/master/AV.db
+	xhr.open('GET', './AV.db', true);
+	xhr.responseType = 'arraybuffer';
+
+	xhr.onload = e => {
+		const uInt8Array = new Uint8Array(xhr.response);
+		const db = new SQL.Database(uInt8Array);
+		const contents = db.exec("SELECT * FROM AVdb limit 10, 10");
+		for (i in contents[0]['values']) {
+			output += '<img src="' + contents[0]['values'][i][3] + '"/>' + "\n";
+		}
+		inner.innerHTML = output;
+	};
+	xhr.send();
+
+});
+
+
